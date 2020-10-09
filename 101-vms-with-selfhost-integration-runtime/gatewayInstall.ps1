9460aaf55c2cd5eebddf8c13246adf752e37c894
jeroenski74 <jeroenski74@hotmail.com>
2020-10-09 13:25:31 +0200

    Update gatewayInstall.ps1
    Added Java Installation

--- 101-vms-with-selfhost-integration-runtime/gatewayInstall.ps1
+++ 101-vms-with-selfhost-integration-runtime/gatewayInstall.ps1
@@ -179,3 +179,7 @@ Install-Gateway $gwPath
 
 Register-Gateway $gatewayKey
 
+$URL=(Invoke-WebRequest -UseBasicParsing https://www.java.com/en/download/manual.jsp).Content | %{[regex]::matches($_, '(?:<a title="Download Java software for Windows Online" href=")(.*)(?:">)').Groups[1].Value}
+Invoke-WebRequest -UseBasicParsing -OutFile jre8.exe $URL
+Start-Process .\jre8.exe '/s REBOOT=0 SPONSORS=0 AUTO_UPDATE=0' -wait
+echo $?
